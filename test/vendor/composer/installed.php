7a1d2991c686643f3386ba76d2823357c7194c8a
jahongi2503 <jojo.isomidinov@gmail.com>
2024-04-09 22:19:40 +0500

    fix

---
+++ test/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jahongir2503/php-prakrik',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jahongir2503/php-prakrik' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
